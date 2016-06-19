7b4b63a52e44a022e7de033c40ebfe96f1443a83
evoup <evoex123@gmail.com>
2016-06-19 22:14:40 +0800

    some bug that can not forward to activity02

--- app/src/main/java/com/evoupsight/tspclient/ResultActivity.java
+++ app/src/main/java/com/evoupsight/tspclient/ResultActivity.java
@@ -1,9 +1,15 @@
 package com.evoupsight.tspclient;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by evoup on 16-6-19.
  */
 public class ResultActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_main);
+    }
 }
