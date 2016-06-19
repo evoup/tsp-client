9a9d7abae5edc3c654c05b15ef1c3b9eedddc454
evoup <evoex123@gmail.com>
2016-06-19 15:40:59 +0800

    add source

---
+++ app/src/main/java/com/evoupsight/tspclient/MainActivity.java
@@ -0,0 +1,13 @@
+package com.evoupsight.tspclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
